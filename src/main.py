4a5ae9131205c90e9679002ffa23e9d78ec8824a
darha <darhanuva@gmail.com>
2023-03-20 13:57:47 +0600

    main api

--- src/main.py
+++ src/main.py
@@ -1,11 +1,51 @@
 from fastapi import FastAPI, Request
 
+from db.database import engine
+from db.models import RequestLog
+
+from typing import Optional
+from sqlalchemy import desc
+from sqlalchemy.orm import Session
+from sqlalchemy.exc import SQLAlchemyError
+
+from utils.weather_api_utils import get_weather_by_date
+
 
 app = FastAPI()
 
 
 @app.get("/weather/{date}")
-async def get_weather_by_date(date: str, request: Request):
-    client_host = request.client.host
-    print(request)
-    return {"client_host": client_host, "date": date}
+async def get_weather(date: str):
+    service_response = await get_weather_by_date(date)
+    base_response = service_response["days"][0]
+
+    return base_response
+
+
+@app.get("/requests")
+async def get_requests(limit: Optional[int] = 100):
+    with Session(engine) as session:
+        requests = session.query(RequestLog).order_by(desc(RequestLog.date)).limit(limit).all()
+
+        requests_dict = []
+        for i in requests:
+            requests_dict.append({col.name: getattr(i, col.name) for col in i.__table__.columns})
+
+        return requests_dict
+
+
+@app.middleware("http")
+async def add_request_log(request: Request, call_next):
+    remote_addr = request.client.host
+    query_string = str(request.url)
+    response = await call_next(request)
+
+    if response.status_code == 200:
+        try:
+            with Session(engine) as session:
+                request_log = RequestLog(ip=remote_addr, query=query_string)
+                session.add(request_log)
+                session.commit()
+        except SQLAlchemyError as e:
+            print(e)
+    return response
--- src/utils/weather_api_utils.py
+++ src/utils/weather_api_utils.py
@@ -1,9 +1,12 @@
-import request
+import aiohttp
 
 
-def get_weather_by_date(date: str):
+async def get_weather_by_date(date: str):
+    session = aiohttp.ClientSession()
+
     # date example - 2023-03-19
     url = f"https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/moscow/{date}/{date}?unitGroup=metric&include=days&key=LZNGP9R5WS55CXBTUREGA5S5N&contentType=json"
-    response = request.get(url).text
+    async with session.get(url=url) as response:
+        response_text = await response.json()
 
-    return response
+    return response_text
